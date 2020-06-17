3441f9eceb0070f5d7655d3ecf8d6f11a97b294c
Xiangdong Chen <xiangdc@uci.edu>
2020-06-17 10:33:52 -0400

    i

--- PS4/app.js
+++ PS4/app.js
@@ -5,7 +5,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const PS4Router = require('./routes/PS4');
 
 const app = express();
@@ -21,7 +20,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/PS4', PS4Router);
 
 // catch 404 and forward to error handler
--- PS4/routeText/distance.js
+++ PS4/routeText/distance.js
@@ -8,22 +8,22 @@ const {describe} = mocha;
 chai.use(chaiHttp);
 
 describe('Distance API', () =>{
-    it('should return 200 success code', function (done) {
+    it('should return 404 ', function (done) {
         chai.request(app)
-            .get('/')
+            .get('/PS4/')
             .end((err,response) => {
+                expect(response).to.have.status(404);
+                done();
+            })
+    }),
+    it('should return 200', function (done) {
+        chai.request(app)
+            .post('/PS4')
+            .end((err,response,body) => {
                 expect(response).to.have.status(200);
                 done();
             })
     }),
-    it('should return 404', function (done) {
-        chai.request(app)
-            .post('/')
-            .end((err,response,body) => {
-                expect(response).to.have.status(404);
-                done();
-            })
-    })
     it('should has from Irvine to Boston', function (done) {
         chai.request(app)
             .post('/PS4/')
@@ -34,5 +34,16 @@ describe('Distance API', () =>{
                 expect(response.request._data.to).to.equal("Boston");
                 done();
             })
-    }).timeout(10000)
+    }).timeout(20000),
+//This timeout thing is just because I am in China, the connection is kinda slow.
+    it('should has method post', function (done) {
+        chai.request(app)
+            .post('/PS4/')
+            .send({from:"Irvine"})
+            .send({to:"Boston"})
+            .end((err,response,body) => {
+                expect(response.request.method).to.equal("post");
+                done();
+            })
+    }).timeout(20000)
 })
